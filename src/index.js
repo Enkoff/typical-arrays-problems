a164823fa65b268170af5ef7171f9c76f1307126
Enkoff <0639818413@ukr.net>
2020-09-29 09:00:07 +0300

    fix:function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 
+
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  } else {
+    if (array.length == 0) {
+      return 0;
+    } else {
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (min > array[i]) {
+        min = array[i];
+      }  
+    }
+    return min;
+   }
+  } 
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array === undefined) {
+    return 0;
+  } else {
+    if (array.length == 0) {
+      return 0;
+  } else {
+   let max = array[0];
+   for (let i = 0; i < array.length; i++) {
+     if (max < array[i]) {
+       max = array[i];
+      }  
+    }
+   return max;
+   }
+ } 
+} 
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  } else {
+    if (array.length == 0) {
+      return 0;
+  } else {
+    let avg = 0;
+    for (let i = 0; i < array.length; i++) {
+      avg += array[i]; 
+    }
+    return avg / array.length;
+   }
+ }
 }
+
